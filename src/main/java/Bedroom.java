80ecf4cd86683feac4830667882af3341302b6df
L3n1ad <danieltoth0910@gmail.com>
2020-01-08 11:27:27 +0000

    create bedroom guest

--- src/main/java/Bedroom.java
+++ src/main/java/Bedroom.java
@@ -1,2 +1,41 @@
+import java.util.ArrayList;
+
 public class Bedroom {
+
+    private int roomNumber;
+    private int capacity;
+    private ArrayList<Guest> guests;
+    private String type;
+
+    public Bedroom(int roomNumber, int capacity, String type){
+        this.roomNumber = roomNumber;
+        this.capacity = capacity;
+        this.guests = new ArrayList<Guest>();
+        this.type = type;
+    }
+
+    public int getRoomNumber() {
+        return this.roomNumber;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public String getType() {
+        String type = this.type;
+        return type;
+    }
+
+    public int guestCount() {
+        return this.guests.size();
+    }
+
+    public void addGuest(Guest guest) {
+        this.guests.add(guest);
+    }
+
+    public void removeGuest() {
+        this.guests.clear();
+    }
 }
--- src/test/java/BedroomTest.java
+++ src/test/java/BedroomTest.java
@@ -1,2 +1,57 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BedroomTest {
+
+    private Bedroom bedroom;
+    private Guest guest;
+
+    @Before
+    public void before(){
+        bedroom = new Bedroom(102, 2, "Double");
+        guest = new Guest("Eugene Kim");
+    }
+
+    @Test
+    public void canGetRoomNumber(){
+        assertEquals(102, bedroom.getRoomNumber());
+    }
+
+    @Test
+    public void canGetRoomCapacity(){
+        assertEquals(2, bedroom.getCapacity());
+    }
+
+    @Test
+    public void canGetRoomType(){
+        assertEquals("Double", bedroom.getType());
+    }
+
+    @Test
+    public void roomStartsEmpty(){
+        assertEquals(0, bedroom.guestCount());
+    }
+
+    @Test
+    public void canAddGuest(){
+        bedroom.addGuest(guest);
+        assertEquals(1, bedroom.guestCount());
+    }
+
+    @Test
+    public void canRemoveGuest(){
+        bedroom.addGuest(guest);
+        bedroom.removeGuest();
+        assertEquals(0, bedroom.guestCount());
+    }
+
+    @Test
+    public void cantRemoveGuest(){
+        bedroom.removeGuest();
+        assertEquals(0, bedroom.guestCount());
+    }
+
+
 }
